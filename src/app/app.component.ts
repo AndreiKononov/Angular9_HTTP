5a74276687ea49893cb665c3de3b839d9af6b035
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-04-06 00:48:46 +0300

    POST requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpService } from './http.service';
 import { User } from './user';
 
 @Component({
     selector: 'my-app',
     template: `
-        <div>{{error}}</div>
-        <ul>
-            <li *ngFor="let user of users">
-                <p>Имя пользователя: {{user?.name}}</p>
-                <p>Возраст пользователя: {{user?.age}}</p>
-            </li>
-        </ul>
+        <div class="form-group">
+            <label>Name</label>
+            <input class="form-control" name="username" [(ngModel)]="user.name" />
+        </div>
+        <div class="form-group">
+            <label>Age</label>
+            <input class="form-control" type="number" name="age" [(ngModel)]="user.age" />
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" (click)="submit(user)">Submit</button>
+        </div>
+        <div *ngIf="done">
+            <div>Received from server:</div>
+            <div>Name: {{receivedUser.name}}</div>
+            <div>Age: {{receivedUser.age}}</div>
+        </div>
     `,
     providers: [ HttpService ],
 })
+export class AppComponent {
 
-export class AppComponent implements OnInit {
+    user: User = new User(); // data of entered user
 
-    users: User[] = [];
-    error: any;
+    receivedUser: User; // received user
+    done: boolean = false;
     constructor(private httpService: HttpService) { }
-
-    ngOnInit() {
-        this.httpService.getUsers().subscribe(
-            data => this.users = data,
-        //     error => {
-        //     this.error = error.message;
-        //     console.log(error);
-        // }
-        );
+    submit(user: User){
+        this.httpService.postData(user)
+            .subscribe(
+                (data: User) => {this.receivedUser = data; this.done = true;},
+                error => console.log(error)
+            );
     }
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,28 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from './user';
-import { Observable, throwError } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class HttpService {
 
     constructor(private http: HttpClient) { }
 
-    getUsers() : Observable<User[]> {
-        return this.http.get('assets/userss.json')
-            .pipe(map(data => {
-                let userList = data["userList"];
-                return userList.map(function(user: any) {
-                    return {
-                        name: user.userName,
-                        age: user.userAge
-                    };
-                });
-            }),
-                catchError(err => {
-                    console.log(err);
-                    return throwError(err);
-            }))
-    };
+    postData(user: User) {
+        // const body = {
+        //     name: user.name,
+        //     age: user.age
+        // };
+        // return this.http.post('http://localhost:3000/postuser', body);
+
+        // return this.http.post('http://localhost:3000/postuser', user);
+
+        const myHeaders = new HttpHeaders().set('Authorization', 'my-auth-token');
+        return this.http.post('http://localhost:3000/postuser', user, {headers: myHeaders});
+    }
 }
