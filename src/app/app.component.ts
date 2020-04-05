8f16d11bb1f9f1ed81da4d65cc063b64487ad836
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-04-05 14:59:45 +0300

    Usage of http.get()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 
 @Component({
     selector: 'my-app',
-    template: ``
+    template: `
+        <div>
+            <p>User name: {{user?.name}}</p>
+            <p>User age: {{user?.age}}</p>
+        </div>
+    `
 })
 
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+    user: User;
+
+    constructor(private http: HttpClient) { }
+
+    // We gets data from service
+    ngOnInit() {
+        this.http.get('assets/user.json').subscribe((data: User) => this.user = data);
+    }
+}
